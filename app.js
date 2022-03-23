9c1c4613740e6470b1b7ee092f65a5e82a2b9170
Divya <divyawaghmare2@gmail.com>
2022-03-23 12:16:46 +0530

    tanufirst

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const adminRouter = require("./routes/admin.route");
+const userRouter = require("./routes/user.route");
+const mongoose = require("mongoose");
+mongoose.connect(
+  "mongodb+srv://root:root@cluster0.8em2i.mongodb.net/BookMyMeal?retryWrites=true&w=majority"
+);
+
+const port = process.env.PORT || 5555;
+
+const bodyParser = require("body-parser");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+module.exports = mongoose.connection;
+
+app.use("/api/admin", adminRouter);
+app.use("/api/user", userRouter);
+
+app.listen(port, () => {
+  console.log("server Started");
+});
